2a612cb8f835fe6ebaee62b8b47375697f11d5e9
syune-ghazaryan <1ghazaryansyune@gmail.com>
2017-05-29 02:41:11 +0400

    Add files via upload

---
+++ Data.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SendImageByJson
+{
+
+    public class Data
+    {
+       
+        public string Name { get; set; }
+      
+        public byte[] Image { get; set; }
+
+    }
+
+}
---
+++ IndexController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SendImageByJson.Controllers
+{
+    public class IndexController : ApiController
+    {
+      
+        [HttpPost]
+        [Route("api/Index/DataSender")]
+        public HttpResponseMessage DataSender(string name, [FromBody]Data image)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK,
+              new
+              {
+                  Name = image.Name,
+                  Image= image.Image
+            });
+        }
+    }
+
+}
+
